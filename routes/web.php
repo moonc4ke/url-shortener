667f9fdc34443e40b13ff7299304ab9c8d8db559
mooncake <adom.donatas@gmail.com>
2024-06-07 19:29:29 +0300

    Remove unused files

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\UrlController;
 
 Route::get('/', [UrlController::class, 'index'])->name('home');
 Route::post('/shorten', [UrlController::class, 'shorten']);
 Route::get('/{hash}', [UrlController::class, 'redirect']);
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
