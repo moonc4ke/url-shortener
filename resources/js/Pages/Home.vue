b715f82b1a18462f43634c637cee98df3436c69a
mooncake <adom.donatas@gmail.com>
2024-06-07 19:22:22 +0300

    Improve prototype styles

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -31,20 +31,22 @@ const submit = () => {
 </script>
 
 <template>
-    <div>
-        <form @submit.prevent="submit">
+    <div class="m-4">
+        <form @submit.prevent="submit" class="mb-4">
             <input v-model="url" type="url" placeholder="Enter URL" required />
-            <button type="submit">Shorten</button>
+            <button
+                class="ml-2 rounded-lg px-4 py-2 border-2 border-blue-500 text-blue-500 hover:bg-blue-600 hover:text-blue-100 duration-300"
+                type="submit"
+            >
+                Shorten
+            </button>
         </form>
         <p v-if="shortUrl">
-            Short URL: <a :href="shortUrl" target="_blank">{{ shortUrl }}</a>
+            Short URL:
+            <a :href="shortUrl" target="_blank" class="hover:underline">{{
+                shortUrl
+            }}</a>
         </p>
-        <div v-if="errors.url" class="error">{{ errors.url }}</div>
+        <div v-if="errors.url" class="text-red-600">{{ errors.url }}</div>
     </div>
 </template>
-
-<style scoped>
-.error {
-    color: red;
-}
-</style>
